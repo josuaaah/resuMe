1b85f53c0d488e7939a4475822a45b7f2913aa66
Joshua Chew Jian Xiang <59608088+josuaaah@users.noreply.github.com>
2021-05-08 22:39:15 +0800

    Implement generation of .txt resumes

--- routes/addNewResume.js
+++ routes/addNewResume.js
@@ -2,6 +2,7 @@ var express = require("express");
 const bodyParser = require("body-parser");
 const { check, validationResult } = require('express-validator');
 var router = express.Router();
+const fs = require('fs');
 
 import { Achievement } from '../models/achievement';
 
@@ -11,7 +12,24 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post('/', async (req, res) => {
-    res.send(req.body)
+    const resumeName = req.body.resumeName;
+    let resumeText = "";
+    const selectedIds = req.body.selectedIds;
+    if (typeof selectedIds === "string") {
+        const selectedAchievement = await Achievement.findById(selectedIds);
+        resumeText += selectedAchievement.name + '\n'
+            + selectedAchievement.description + '\n\n';
+    } else {
+        for (let id of req.body.selectedIds) {
+            const selectedAchievement = await Achievement.findById(id);
+            resumeText += selectedAchievement.name + '\n'
+                + selectedAchievement.description + '\n\n';
+        }
+    }
+    fs.writeFile(resumeName + '.txt', resumeText, function (err) {
+        if (err) return console.log(err);
+    });
+    res.redirect('/myResumes');
 })
 
 module.exports = router;
